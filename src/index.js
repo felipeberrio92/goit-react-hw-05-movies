1d7745a75a5e9a07a3113d93dd3669d2f158b77c
felipeberrio92 <felipeberriomarin92@gmail.com>
2024-05-04 19:48:33 -0500

    se corrige enrutamiento

--- src/index.js
+++ src/index.js
@@ -5,15 +5,25 @@ import { RouterProvider } from 'react-router-dom';
 import Routes from './routes/Routes';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+/*import { BrowserRouter } from 'react-router-dom';*/
 
 const basename = '/goit-react-hw-05-movies/';
 <Router basename={basename}>
   {ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Router basename={basename}></Router>
     <RouterProvider router={Routes}>
       <App />
     </RouterProvider>
   </React.StrictMode>
 )}
 </Router>
+
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <BrowserRouter   basename="/goit-react-hw-05-movies/" >
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);*/
